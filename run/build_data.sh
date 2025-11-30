e17040676402c79634ebcd17ecda2615bce2de13
lcxlcx <33259639+lcxlcx@users.noreply.github.com>
2025-11-30 18:05:37 +0800

    Delete run directory

--- run/build_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-data_path="/root/data/"
-
-python build_data.py \
-        --input-dir $data_path/ \
-        --output-dir /root/data/databanks/ \
-        --vocab-file /root/data/vocab.json
